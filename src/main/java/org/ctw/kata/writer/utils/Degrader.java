5a4eab0d1498b943355750079ca08633c48cca0b
Charlie Winnard <ctwinnard@gmail.com>
2017-04-12 22:26:39 -0400

    Added business logic that checks if a character of the string is empty.  If it is, don't assign a value.  If it's not, increment the counter.  Then return the counter.

--- src/main/java/org/ctw/kata/writer/utils/Degrader.java
+++ src/main/java/org/ctw/kata/writer/utils/Degrader.java
@@ -2,8 +2,16 @@ package org.ctw.kata.writer.utils;
 
 public class Degrader {
 
-	public int countInputLength(String string) {
-		return string.length();
+	public int countInputLength(String input) {
+		int weightedLength = 0;
+		char[] characters = input.toCharArray();
+		for (char c: characters) {
+			if (c == ' ') {
+				continue;
+			}
+			weightedLength++;
+		}
+		return weightedLength;
 	}
 	
 }
