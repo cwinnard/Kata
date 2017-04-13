cb917c528a3b06650fe8cbb84d3b7fe13aac84bf
Charlie Winnard <ctwinnard@gmail.com>
2017-04-12 22:22:09 -0400

    Adding test that sets up expectation that no value is assigned to spaces.  Failing because logic is still only checking raw length

--- src/test/java/org/ctw/kata/writer/utils/DegraderTest.java
+++ src/test/java/org/ctw/kata/writer/utils/DegraderTest.java
@@ -3,6 +3,8 @@ package org.ctw.kata.writer.utils;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+import java.awt.datatransfer.StringSelection;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -16,5 +18,11 @@ public class DegraderTest {
 	public void testCountsInput() throws Exception {
 		int stringLength = degrader.countInputLength("12345");
 		assertThat(stringLength, is(5));
+	}
+	
+	@Test
+	public void testCountsInput_Assigns0ToSpaces() throws Exception {
+		int lengthSansSpaces = degrader.countInputLength("123 456");
+		assertThat(lengthSansSpaces, is(6));
 	}
 }
